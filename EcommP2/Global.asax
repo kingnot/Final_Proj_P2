9ac2485204e246303d93733d3f5e25124e09065b
mayankraval <mayankraval@hotmail.com>
2016-08-11 22:24:46 -0400

    Add project files.

---
+++ EcommP2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EcommP2.MvcApplication" Language="C#" %>
---
+++ EcommP2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EcommP2.Startup))]
+namespace EcommP2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
