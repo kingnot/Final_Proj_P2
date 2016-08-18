41a7d9b1c3d798c19c56d5127031be1ce9159616
Fei Wang <kingnot0524@gmail.com>
2016-08-18 14:59:57 -0400

    Editted style for Cart page, finally figure out the crash issue

---
+++ DropAndCreate dbo.Carts.sql
@@ -0,0 +1,23 @@
+﻿USE [FinalDB]
+GO
+
+/****** Object: Table [dbo].[Carts] Script Date: 08/18/2016 2:46:36 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[Carts];
+
+
+GO
+CREATE TABLE [dbo].[Carts] (
+    [RecordId]    INT Identity (1,1)         NOT NULL Primary key,
+    [CartId]      VARCHAR (50) NOT NULL,
+    [Count]       INT          NOT NULL,
+    [DateCreated] DATETIME     NOT NULL,
+    [foodID]      INT          NOT NULL
+);
+
+
